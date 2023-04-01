9f4bc084e9915d36b45c173aa432f94ef9fc5a33
ravi kumar dhawan <ravidhawan8899@gmail.com>
2023-04-01 17:19:53 +0530

    initial commit

---
+++ src/components/Home.tsx
@@ -0,0 +1,14 @@
+import { Heading } from "@chakra-ui/react";
+
+function Home() {
+  return (
+    <div>
+      <Heading textAlign={"center"} bg={"green"}>
+        {" "}
+        YUP AND REACT QUERY TUTORIAL{" "}
+      </Heading>
+    </div>
+  );
+}
+
+export default Home;
